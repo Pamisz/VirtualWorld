aa4b81fc0b711f5f01dc3890286b6f6bca6c6fbe
Pamisz <131891673+Pamisz@users.noreply.github.com>
2023-04-26 19:52:13 +0200

    Delete WirtualnyŚwiat directory

--- WirtualnyŚwiat/organizm.cpp
+++
@@ -1,8 +0,0 @@
-#include "organizm.h"
-
-//organizm::organizm(int sila, int inicjatywa, int polozenieX, int polozenieY, swiat s)
-//	:sila(sila), inicjatywa(inicjatywa), s(s)
-//{
-//	polozenie[0] = polozenieX;
-//	polozenie[1] = polozenieY;
-//}
